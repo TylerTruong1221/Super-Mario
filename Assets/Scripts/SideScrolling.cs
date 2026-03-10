af639269862d781e8e4b66efdb7065920f0572d1
hnyuh <jasonhuynh.dev@gmail.com>
2026-03-09 17:42:22 -0700

    Imeplement pipes and camera transitions

--- Assets/Scripts/SideScrolling.cs
+++ Assets/Scripts/SideScrolling.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class SideScrolling : MonoBehaviour
 {
     private Transform player;
+    public float height = 7.0f;
+    public float undergroundHeight = -8.0f;
     
     private void Awake()
     {
@@ -13,6 +15,13 @@ public class SideScrolling : MonoBehaviour
     {
         Vector3 cameraPosition = transform.position;
         cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+        transform.position = cameraPosition;
+    }
+
+    public void SetUnderground(bool underground)
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.y = underground ? undergroundHeight : height;
         transform.position = cameraPosition;
     }
 }
